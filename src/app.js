bdccafa7d755ee0edefdf85091d3e82f18471d26
Alejandrobv25 <alexbavierasaiz@gmail.com>
2024-11-23 13:42:28 +0000

    last commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["her", "your", "his", "their", "our"];
+  let adj = ["amazing", "strange", "random", "cute"];
+  let noun = ["jogger", "racoon", "thoughts", "shoes"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let x = 0; x < noun.length; x++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[x]}.com`);
+      }
+    }
+  }
 };
